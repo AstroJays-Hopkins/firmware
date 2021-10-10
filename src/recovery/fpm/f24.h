aab3e65786105c9c94fe29f6a0a8afc183c05f49
theo <theoleehusky@gmail.com>
2021-10-10 16:00:25 -0400

    fpm is q16_15_t

--- src/recovery/fpm/f24.h
+++ src/recovery/fpm/f24.h
@@ -1,13 +1,13 @@
 #include <stdint.h>
 
-typedef uint32_t f24_t; 
+typedef uint32_t q16_15_t; 
 
-f24_t add(f24_t, f24_t);
-f24_t subtract(f24_t, f24_t);
-f24_t multiply(f24_t, f24_t);
-f24_t divide(f24_t, f24_t);
+q16_15_t add(q16_15_t, q16_15_t);
+q16_15_t subtract(q16_15_t, q16_15_t);
+q16_15_t multiply(q16_15_t, q16_15_t);
+q16_15_t divide(q16_15_t, q16_15_t);
 
-f24_t tofixed(uint32_t);
+q16_15_t tofixed(uint32_t);
 
-uint32_t extractint(f24_t);
-uint32_t extractfrac(f24_t);
+uint32_t extractint(q16_15_t);
+uint32_t extractfrac(q16_15_t);
