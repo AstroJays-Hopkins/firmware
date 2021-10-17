a6fd051f4ce4c3f99ec67d4889f10adfed7c645a
theo <theoleehusky@gmail.com>
2021-10-17 15:46:40 -0400

    add explicit cast

--- src/recovery/fpa/fpa.c
+++ src/recovery/fpa/fpa.c
@@ -17,7 +17,7 @@ q16_16_t divide(q16_16_t a, q16_16_t b) {
 }
 
 q16_16_t tofpa(uint16_t integer) {
-	return integer << 16;
+	return (q16_16_t)integer << 16;
 }
 
 q16_16_t partstofpa(uint16_t integer, uint16_t fraction) {
