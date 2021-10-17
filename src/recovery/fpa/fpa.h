6670ae9eb2d55cad03c731faa04e98256965e3f4
theo <theoleehusky@gmail.com>
2021-10-17 14:57:05 -0400

    fix comments

--- src/recovery/fpa/fpa.h
+++ src/recovery/fpa/fpa.h
@@ -3,14 +3,18 @@
 
 #include <stdint.h>
 
+// unsigned fixed point arithmetic implementation with 2^-16 precision
 typedef uint32_t q16_16_t;
 
+// TODO: implement overflow
 q16_16_t add(q16_16_t, q16_16_t);
 q16_16_t subtract(q16_16_t, q16_16_t);
 q16_16_t multiply(q16_16_t, q16_16_t);
 q16_16_t divide(q16_16_t, q16_16_t);
 
+// int to fpa
 q16_16_t tofpa(uint16_t);
+// int + frac to fpa
 q16_16_t partstofpa(uint16_t, uint16_t);
 
 uint16_t extractint(q16_16_t);
