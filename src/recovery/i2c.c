ab4f45250f39b1caf92c0fa6b7092a271276b18e
cometku <56900136+cometku@users.noreply.github.com>
2021-10-19 15:06:06 -0400

    USART: Added basic functionalities for interrupts. I2C: Added Stop and Start Conditons and Syncbusies

--- src/recovery/i2c.c
+++ src/recovery/i2c.c
@@ -1,22 +1,37 @@
 #include "sam.h"
 #include "globals.h"
 
+//TODO Ask if I have to handle ACK and NACK or should i rely on smart enable doing ACK and NACK
+
 
 //defined in 28.6.2.1
 void init_i2c(int lowtout, int baud) {
     //lowtout 0 or 1
+    PM_REGS->PM_APBCMASK |= PM_APBCMASK_SERCOM0(0x1);
     SERCOM0_REGS->I2CM.SERCOM_CTRLA = SERCOM_I2CM_CTRLA_MODE_I2C_MASTER| //setting it as master
                                       SERCOM_I2CM_CTRLA_INACTOUT_205US| //setting the inactive time out to the highest possible
                                       SERCOM_I2CM_STATUS_LOWTOUT(lowtout); //setting the low
-    SERCOM0_REGS->I2CM.SERCOM_CTRLB |= SERCOM_I2CM_CTRLB_SMEN(1); //setting smart enable
+    SERCOM0_REGS->I2CM.SERCOM_CTRLB |= SERCOM_I2CM_CTRLB_SMEN(0x1); //setting smart enable
+    
+    
     SERCOM0_REGS->I2CM.SERCOM_BAUD = (sercom_clock/(2*baud))-1; //calculating baud rate for sync
-    SERCOM0_REGS->I2CM.SERCOM_CTRLA |= SERCOM_I2CM_CTRLA_ENABLE(1); //enabling the serial protocol
+    SERCOM0_REGS->I2CM.SERCOM_CTRLA |= SERCOM_I2CM_CTRLA_ENABLE(0x1); //enabling the serial protocol
+    while(_FLD2VAL(SERCOM_I2CM_SYNCBUSY_ENABLE, SERCOM0_REGS->I2CM.SERCOM_SYNCBUSY));
+    SERCOM0_REGS->I2CM.SERCOM_STATUS = SERCOM_I2CM_STATUS_BUSSTATE(0x1);
+    while(_FLD2VAL(SERCOM_I2CM_SYNCBUSY_SYSOP, SERCOM0_REGS->I2CM.SERCOM_SYNCBUSY));
 }
 //defined in 28.6.2.4.2
 int transmit_address(int read_or_write, int addr) {
     //read = 1
     //write = 0
+    //checking if the BUS is busy
+    while(_FLD2VAL(SERCOM_I2CM_STATUS_BUSSTATE, SERCOM0_REGS->I2CM.SERCOM_STATUS)==0x3);
+    //send start condition
+    SERCOM0_REGS->I2CM.SERCOM_STATUS |= SERCOM_I2CM_CTRLB_CMD(0x1);
+    while(_FLD2VAL(SERCOM_I2CM_SYNCBUSY_SYSOP, SERCOM0_REGS->I2CM.SERCOM_SYNCBUSY));
+    //write address
     SERCOM0_REGS->I2CM.SERCOM_ADDR = SERCOM_I2CM_ADDR_ADDR(addr| read_or_write); //sending the address of the peripheral with a read or write bit ORed on
+    while(_FLD2VAL(SERCOM_I2CM_SYNCBUSY_SYSOP, SERCOM0_REGS->I2CM.SERCOM_SYNCBUSY));
     int master_on_bus = _FLD2VAL(SERCOM_I2CM_INTFLAG_MB, SERCOM0_REGS->I2CM.SERCOM_INTFLAG); //checking for master_on_bus bit set
     int arb_loss_error = _FLD2VAL(SERCOM_I2CM_STATUS_ARBLOST, SERCOM0_REGS->I2CM.SERCOM_STATUS); //checking for arbitration loss error
     int bus_error  = _FLD2VAL(SERCOM_I2CM_STATUS_BUSERR, SERCOM0_REGS->I2CM.SERCOM_STATUS); //checking for master bus error
@@ -47,16 +62,24 @@ int transmit_address(int read_or_write, int addr) {
 //defined in 28.6.2.4.4 and 28.6.2.4.5
 int receive_i2c() {
     //read data in from data register
-    int data = _FLD2VAL(SERCOM_I2CM_DATA_DATA, SERCOM0_REGS->I2CM.SERCOM_DATA);
+    int data = SERCOM0_REGS->I2CM.SERCOM_DATA;
+    //send end condition
+    SERCOM0_REGS->I2CM.SERCOM_STATUS |= SERCOM_I2CM_CTRLB_CMD(0x3);
+    while(_FLD2VAL(SERCOM_I2CM_SYNCBUSY_SYSOP, SERCOM0_REGS->I2CM.SERCOM_SYNCBUSY));
+
     return data;
 }
 //defined in 28.6.2.4.3
 int transmit_i2c(int write_data) {
     //write to register
     SERCOM0_REGS->I2CM.SERCOM_DATA = write_data;
+    while(_FLD2VAL(SERCOM_I2CM_SYNCBUSY_SYSOP, SERCOM0_REGS->I2CM.SERCOM_SYNCBUSY));
     //wait till arbitration loss and rxnack are not set
     //must wait for rxnack to not be set 
-    while(!_FLD2VAL(SERCOM_I2CM_STATUS_ARBLOST, SERCOM0_REGS->I2CM.SERCOM_STATUS)&&!_FLD2VAL(SERCOM_I2CM_STATUS_RXNACK, SERCOM0_REGS->I2CM.SERCOM_STATUS));
+    while(!_FLD2VAL(SERCOM_I2CM_STATUS_RXNACK, SERCOM0_REGS->I2CM.SERCOM_STATUS));
+    //send end condition
+    SERCOM0_REGS->I2CM.SERCOM_STATUS |= SERCOM_I2CM_CTRLB_CMD(0x3);
+    while(_FLD2VAL(SERCOM_I2CM_SYNCBUSY_SYSOP, SERCOM0_REGS->I2CM.SERCOM_SYNCBUSY));
     //if arbitration loss happens error
     if (_FLD2VAL(SERCOM_I2CM_STATUS_ARBLOST, SERCOM0_REGS->I2CM.SERCOM_STATUS)){
         return -1;
--- src/recovery/uart.c
+++ src/recovery/uart.c
@@ -5,6 +5,22 @@
 #include "globals.h"
 #include "pinmap.h"
 
+//GLOBAL DATA FOR INTERRUPT
+static uint8_t global_data = 0; 
+
+void comm_interrupt_init() {
+    NVIC_SetPriority(SERCOM0_IRQn, 2);        // set usart interrupts to 2nd lowest priority
+    NVIC_EnableIRQ(SERCOM0_IRQn);             // enable interrupt
+}
+void SERCOM0_Handler(){
+    //if Recieve complete
+    if (_FLD2VAL(SERCOM_USART_INT_INTFLAG_RXC, SERCOM0_REGS->USART_INT.SERCOM_INTFLAG)){
+        //put data into global 
+        global_data = SERCOM0_REGS->USART_INT.SERCOM_DATA;
+        //clear the interrupt 
+        SERCOM0_REGS->USART_INT.SERCOM_INTENCLR |= SERCOM_USART_INT_INTENCLR_RXC(0x1);
+    }
+}
 //read 26.6.2.1 in the datasheet for more explanation
 void init_uart(const uint32_t target_baud){
     PM_REGS->PM_APBCMASK |= PM_APBCMASK_SERCOM0(0x1);
@@ -20,15 +36,18 @@ void init_uart(const uint32_t target_baud){
     SERCOM0_REGS->USART_INT.SERCOM_CTRLB = SERCOM_USART_INT_CTRLB_CHSIZE_8_BIT         // Configure Charactersize field in CTRLB.CHSIZE
                                            | SERCOM_USART_INT_CTRLB_SBMODE_1_BIT       // CTRLB.SBMODE - # of stop bits
                                            | SERCOM_USART_INT_CTRLB_RXEN(0x1)          // Write 1 to CTRLB.RXEN and CTRLB.TXEN
-                                           | SERCOM_USART_INT_CTRLB_TXEN(0x1)
-                                           /*| SERCOM_USART_INT_CTRLB_SFDE(0x1)*/;
+                                           | SERCOM_USART_INT_CTRLB_TXEN(0x1);
+    //wait for sync busy
     while(_FLD2VAL(SERCOM_USART_INT_SYNCBUSY_CTRLB, SERCOM0_REGS->USART_INT.SERCOM_SYNCBUSY));
+    //activate rxc interrupt
+    SERCOM0_REGS->USART_INT.SERCOM_INTENSET |= SERCOM_USART_INT_INTENSET_RXC(0x1);
     // TODO: implement a FIXED_POINT math library and use for this calculation
     /* SERCOM0_REGS->USART_INT.SERCOM_BAUD = 65536 * (1-16*(target_baud/sercom_clock)); //the fraction is multiplied by 16 which is the sample rate defined in SAMR */
     SERCOM0_REGS->USART_INT.SERCOM_BAUD = 5138; // baud = 115200
     // Note data sheet says that enabling the uart will immediately disable the
     // TX and RX settings, but we must enable them to start the UART.
     SERCOM0_REGS->USART_INT.SERCOM_CTRLA |= SERCOM_USART_INT_CTRLA_ENABLE(0x1);
+    //wait for sync busy 
     while(_FLD2VAL(SERCOM_USART_INT_SYNCBUSY_ENABLE, SERCOM0_REGS->USART_INT.SERCOM_SYNCBUSY));
 }
 
@@ -51,12 +70,7 @@ int transmit_uart(const uint8_t write_data){
 uint8_t receive_uart(){
     SERCOM0_REGS->USART_INT.SERCOM_CTRLB |= SERCOM_USART_INT_CTRLB_RXEN(0x1);
     while(_FLD2VAL(SERCOM_USART_INT_SYNCBUSY_CTRLB, SERCOM0_REGS->USART_INT.SERCOM_SYNCBUSY));
-    /*
-    if (_FLD2VAL(SERCOM_USART_INT_INTFLAG_RXS, SERCOM0_REGS->USART_INT.SERCOM_INTFLAG)){
-        PORT_REGS->GROUP[LED_BANK].PORT_OUT = 0 << LED_PIN;
-    }
-    return 0;
-    */
+    //wait for receive clear 
     while(!_FLD2VAL(SERCOM_USART_INT_INTFLAG_RXC, SERCOM0_REGS->USART_INT.SERCOM_INTFLAG));
     
     int bufovf = _FLD2VAL(SERCOM_USART_INT_STATUS_BUFOVF, SERCOM0_REGS->USART_INT.SERCOM_STATUS);
@@ -67,10 +81,9 @@ uint8_t receive_uart(){
         return -1;
     }
     //reading from data register
-    uint8_t data = _FLD2VAL(SERCOM_USART_INT_DATA_DATA, SERCOM0_REGS->USART_INT.SERCOM_DATA);
+    uint8_t data = SERCOM0_REGS->USART_INT.SERCOM_DATA;
     //ending recieving
     _FLDCLR(SERCOM0_REGS->USART_INT.SERCOM_CTRLB, SERCOM_USART_INT_CTRLB_RXEN);
     while(_FLD2VAL(SERCOM_USART_INT_SYNCBUSY_CTRLB, SERCOM0_REGS->USART_INT.SERCOM_SYNCBUSY));
     return data;
-    
 }
